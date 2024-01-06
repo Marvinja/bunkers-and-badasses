04d8b030d1613c090a0dbdb018c66993af1df2fa
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2024-01-07 07:31:02 +1100

    fix history list names to exclude extra element dice

--- src/app/history-list/history-list.component.ts
+++ src/app/history-list/history-list.component.ts
@@ -20,16 +20,39 @@ export class HistoryListComponent {
   }
 
   handleLoadGun(item: string) {
-    console.log(item);
     this.loadGun.emit(this.data[parseInt(item)]);
   }
 
   convertElement(element: string) {
     switch(element) {
+      case "Incendiary (+1d6)": 
+        return "Incendiary";
+      case "Incendiary (+2d6)": 
+        return "Incendiary";
+      case "Shock (+1d6)": 
+        return "Shock";
+      case "Shock (+2d6)": 
+        return "Shock";
+      case "Corrosive (+1d6)": 
+        return "Corrosive";
+      case "Corrosive (+2d6)": 
+        return "Corrosive";
+      case "Cryo (+1d6)": 
+        return "Cryo";
+      case "Cryo (+2d6)": 
+        return "Cryo";
+      case "Radiation (+1d6)": 
+        return "Radiation";
+      case "Radiation (+2d6)": 
+        return "Radiation";
+      case "Explosive (+1d6)": 
+        return "Explosive";
+      case "Explosive (+2d6)": 
+        return "Explosive";
       case "Shock + Corrosive":
         return "Voltcidic";
       case "Radiation + Incendiary":
-        return "Inferadiation";
+        return "Schoradiation";
       case "Explosive + Cryo":
         return "Icesplosion";
       case "N/A":
